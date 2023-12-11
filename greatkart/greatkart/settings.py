2334bf17c106c325340478e7fad6b18b6b526b84
Ayshath Ashfiya A <ayshathashfiyaa@gmail.com>
2023-12-11 12:13:15 +0530

    review and rating

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -126,14 +126,13 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
-STATICFILES_DIRS = [
-    'greatkart/static',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+
 #media files configuration
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 
 from django.contrib.messages import constants as messages
 
--- greatkart/greatkart/urls.py
+++ greatkart/greatkart/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -33,6 +34,7 @@ urlpatterns = [
     #orders
     
     path('orders/',include('orders.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
